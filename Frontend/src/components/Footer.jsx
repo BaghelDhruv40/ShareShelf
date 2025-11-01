206194ccbdfde7a5eaad12ac23ca2cc82e806d39
BaghelDhruv40 <dhruvbaghel40@gmail.com>
2025-11-01 17:33:54 +0530

    Footer Component Added

---
+++ Frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+	return (
+		<footer className="bg-gray-100 py-6 mt-10">
+			<div className="container mx-auto text-center text-gray-600">
+				© {new Date().getFullYear()} ShareShelf | Learn, Share, Grow
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
